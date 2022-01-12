70f61dc78229005e51544e7f22636a965bb4cbd1
llvlldat <minhdat.dongoc@gmail.com>
2022-01-12 15:28:54 +0700

    fix font

--- forumfe/src/App.tsx
+++ forumfe/src/App.tsx
@@ -1,29 +1,17 @@
-
 import Login from './components/auth/Login/Login';
 import { Register } from './components/auth/Register/Register';
-
-import { useContext, useEffect, useState } from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import CreatePost from './components/CreatePost/CreatePost';
 import MyPosts from './components/MyPosts/MyPosts';
 import Profile from './components/Profile/Profile/Profile';
-import { UserContext } from './context/UserContext'
-import { login } from './apis/users-apis'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home/Home';
-import { PostContext } from './context/PostContext';
+import UserProfile from './components/Profile/UserProfile/UserProfile'
 
 function App() {
 
-  const context = useContext(UserContext)
-
-  // useEffect(() => {
-  //   login(data)
-  //     .then((user: User) => context.setUser(user))
-  //     .catch(err => console.error(err))
-  // }, [])
   return (
     <BrowserRouter>
       <Header />
@@ -37,6 +25,7 @@ function App() {
         <Route path="/profile/*" element={<Profile />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/user/:id" element={<UserProfile />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- forumfe/src/index.tsx
+++ forumfe/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { UserContextProvider } from './context/UserContext';
 import AuthContextProvider from './context/AuthContext';
 import './index.css';
