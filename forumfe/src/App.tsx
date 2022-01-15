b81eeb3a1297a5195130726f5538012641a21bbb
toan <78101270+Ikaros-lg@users.noreply.github.com>
2022-01-15 15:42:08 +0700

    toan commit 13 fix header

--- forumfe/src/App.tsx
+++ forumfe/src/App.tsx
@@ -6,27 +6,28 @@ import Navigation from './components/Navigation/Navigation';
 import CreatePost from './components/CreatePost/CreatePost';
 import MyPosts from './components/MyPosts/MyPosts';
 import Profile from './components/Profile/Profile/Profile';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Home from './components/Home/Home';
 import UserProfile from './components/Profile/UserProfile/UserProfile'
 import MyPostDetail from "./components/MyPosts/MyPostDetail/MyPostDetail";
 import Post from './components/PostComponent/Post';
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { PostContext } from './context/PostContext'
 import { getPosts } from './apis/posts-apis';
+import { UserContext } from './context/UserContext';
+import { useParams } from 'react-router-dom';
 
 function App() {
   const postContext = useContext(PostContext);
-
   useEffect(() => {
     getPosts()
       .then((data) => { postContext.setPosts(data) })
       .catch((err: Error) => console.log(err));
   }, []);
+
   return (
     <BrowserRouter>
       <Header />
-      {/* {window.location.pathname !== '/login' && window.location.pathname !== '/register' && <Navigation />} */}
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
@@ -40,10 +41,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/post/:id" element={<Post />} />
-
       </Routes>
-      
-      {/* {window.location.pathname !== '/login' && window.location.pathname !== '/register' && <Footer />} */}
 
     </BrowserRouter>
   )
